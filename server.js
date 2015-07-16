f8a0cee00168f0a8d8a569d5283a3428083278a7
Marian Zagoruiko <marian.zagoruiko@gmail.com>
2015-07-16 17:48:18 +0300

    add socket.io connectivity

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var app = express();
 
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 app.use(express.static('public'));
 
-var server = app.listen(3000, function() {
-  var host = server.address().address;
-  var port = server.address().port;
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
 
-  console.log('Example app listening at http://%s:%s', host, port);
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
